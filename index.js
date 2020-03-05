e8c7ab25c81b8cfaf11a89467f806ab2533c1454
pearpages <pere@soms.cat>
2020-03-05 21:54:48 +0100

    test

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const packageJson = require("./package.json");
+const VERSION = packageJson.version;
 const express = require("express");
 
 const app = express();
@@ -6,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.type("text/plain");
-  res.send("Meadowlark Travel, new version 2.0. Port: " + process.env.PORT);
+  res.send("Wedding backend is up. Version: " + VERSION);
 });
 
 app.get("/about", (req, res) => {
